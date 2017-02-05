6dc6e189860bd2527d4713347101a3e06af0790b
jlainezs <jlainezs@econceptes.com>
2017-02-05 21:07:22 +0100

    Cleanup

---
+++ app/src/main/java/com/example/android/popularmovies/utilities/Network.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 EConceptes. All rights reserved.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ * http://www.econceptes.com
+ */
+
+package com.example.android.popularmovies.utilities;
+
+/**
+ * Created by jlainezs on 05/02/2017 for PopularMovies
+ */
+
+public class Network {
+}
