5eb24bc35b724a754d221a3c82ce47bed1677399
jlainezs <jlainezs@econceptes.com>
2017-02-08 15:37:35 +0100

    Error if it can't get movies list fixed.

--- app/src/main/java/com/example/android/popularmovies/utilities/Network.java
+++ app/src/main/java/com/example/android/popularmovies/utilities/Network.java
@@ -9,9 +9,47 @@
 
 package com.example.android.popularmovies.utilities;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by jlainezs on 05/02/2017 for PopularMovies
  */
 
 public class Network {
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = null;
+        StringBuilder resultStrBuilder = new StringBuilder();
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = urlConnection.getInputStream();
+            BufferedReader inp = new BufferedReader(new InputStreamReader(in));
+            String inputStr;
+
+            while ((inputStr = inp.readLine()) != null){
+                resultStrBuilder.append(inputStr);
+            }
+
+        } catch (IOException e){
+            resultStrBuilder.setLength(0);    // clear the string builder
+            throw e;
+        }
+        finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+        return resultStrBuilder.toString();
+    }
+
 }
--- app/src/main/java/com/example/android/popularmovies/utilities/TheMovieDBAPI.java
+++ app/src/main/java/com/example/android/popularmovies/utilities/TheMovieDBAPI.java
@@ -9,9 +9,83 @@
 
 package com.example.android.popularmovies.utilities;
 
+import android.net.Uri;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
- * Created by jlainezs on 05/02/2017 for PopularMovies
+ * Created by jlainezs on 06/02/2017 for PopularMovies
  */
 
-public class TheMovieDBAPI {
+public class TheMovieDBApi {
+    private final static String THEMOVIEDB_API_BASE_URL = "https://api.themoviedb.org/3";
+    private final static String THEMOVIEDB_IMAGES_BASE_URL = "http://image.tmdb.org/t/p";
+    // "w92", "w154", "w185", "w342", "w500", "w780"
+    private final static String THEMOVIEDB_IMAGES_SIZE = "w500";
+    private final static String THEMOVIEDB_API_KEY = "FILL_YOUR_API_KEY_HERE";
+
+    /**
+     * Gets the popular movies JSON url
+     *
+     * @return URL
+     */
+    public URL getPopularMoviesUrl()
+    {
+        Uri builtUri = Uri.parse(THEMOVIEDB_API_BASE_URL).buildUpon()
+                .appendPath("movie")
+                .appendPath("popular")
+                .appendQueryParameter("api_key", THEMOVIEDB_API_KEY)
+                .build();
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public URL getTopRatedMoviesUrl()
+    {
+        Uri builtUri = Uri.parse(THEMOVIEDB_API_BASE_URL).buildUpon()
+                        .appendPath("movie")
+                        .appendPath("top_rated")
+                .appendQueryParameter("api_key", THEMOVIEDB_API_KEY)
+                .build();
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * Gets an image URL
+     *
+     * @param imageName Name of the image file
+     * @return URL
+     */
+    public URL getMovieImageUrl(String imageName)
+    {
+        Uri imageUri = Uri.parse(THEMOVIEDB_IMAGES_BASE_URL).buildUpon()
+                .appendPath(THEMOVIEDB_IMAGES_SIZE)
+                .appendPath(imageName.replace("/", ""))
+                .build();
+        URL url = null;
+
+        try {
+            url = new URL(imageUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
 }
