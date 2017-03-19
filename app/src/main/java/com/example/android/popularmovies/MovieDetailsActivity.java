dfd85373919c99cd462357e9f3cd99ca68a26adc
jlainezs <jlainezs@econceptes.com>
2017-03-19 10:11:54 +0100

    Layout corrections

--- app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -12,9 +13,9 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.popularmovies.adapters.MovieReviewsAdapter;
 import com.example.android.popularmovies.adapters.MovieVideosAdapter;
 import com.example.android.popularmovies.async.AsyncTaskCompleteListener;
-import com.example.android.popularmovies.async.MoviesFetcher;
 import com.example.android.popularmovies.async.MoviesVideosFetcher;
 import com.example.android.popularmovies.utilities.TMDBApi;
 import com.squareup.picasso.Picasso;
@@ -26,6 +27,7 @@ import java.util.ArrayList;
 public class MovieDetailsActivity extends AppCompatActivity {
     private Movie movie;
     private ArrayList<MovieVideo> movieVideos = new ArrayList<>();
+    private ArrayList<MovieReview> movieComments = new ArrayList<>();
 
     public class FetchMoviesVideosTaskCompleteLister  implements AsyncTaskCompleteListener<ArrayList<MovieVideo>> {
 
@@ -40,6 +42,32 @@ public class MovieDetailsActivity extends AppCompatActivity {
             ListView listView = (ListView) findViewById(R.id.movie_videos_list);
             MovieVideosAdapter adapter = (MovieVideosAdapter) listView.getAdapter();
             adapter.addAll(movieVideos);
+            setListViewHeightBasedOnChildren(listView);
+
+            if (exception != null) {
+                if (exception.getClass() == ConnectException.class){
+                    Toast.makeText(MovieDetailsActivity.this, R.string.no_network_available, Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MovieDetailsActivity.this, R.string.error_message_cant_load_movies, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    public class FetchMoviesCommentsTaskCompleteListener implements AsyncTaskCompleteListener<ArrayList<MovieReview>>{
+        @Override
+        public void onTaskComplete(ArrayList<MovieReview> result, Exception exception)
+        {
+            if (movieComments.size() > 0)
+            {
+                movieComments.clear();
+            }
+
+            movieComments = result;
+            ListView listView = (ListView) findViewById(R.id.movie_videos_list);
+            MovieReviewsAdapter adapter = (MovieReviewsAdapter) listView.getAdapter();
+            adapter.addAll(movieComments);
+            setListViewHeightBasedOnChildren(listView);
 
             if (exception != null) {
                 if (exception.getClass() == ConnectException.class){
@@ -101,5 +129,29 @@ public class MovieDetailsActivity extends AppCompatActivity {
         TMDBApi api = new TMDBApi();
         URL videosUrl = api.getMovieVideos(movie.getId().toString());
         new MoviesVideosFetcher(this, new FetchMoviesVideosTaskCompleteLister()).execute(videosUrl);
+    }
+
+    /**
+     *
+     * @param listView
+     * @link https://kennethflynn.wordpress.com/2012/09/12/putting-android-listviews-in-scrollviews/
+     */
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        MovieVideosAdapter adapter = (MovieVideosAdapter) listView.getAdapter();
+        if (adapter == null) {
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < adapter.getCount(); i++) {
+            View listItem = adapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (adapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
